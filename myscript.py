9f25b82d66136fab2f6022bd59f86cb9e89bc35b
RNavidi <r_navidi@yahoo.com>
2020-02-10 11:27:30 -0800

    fixed the function

--- myscript.py
+++ myscript.py
@@ -3,4 +3,6 @@ def my_func(matrix_1, matrix_2):
     return matrix_1 * matrix_2
 
 my_matrix = np.identity(4)
-my_second_matrix
+my_second_matrix= np.random.random(size=(4,5))
+
+print(my_func(my_matrix, my_second_matrix))
